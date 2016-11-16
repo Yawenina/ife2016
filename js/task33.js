f2a46b63b5bafb8f6f43378641bc0b3e99287c73
Yawenina <xiaoywkate@163.com>
2016-11-16 23:03:41 +0800

    complete task33

--- js/task33.js
+++ js/task33.js
@@ -6,7 +6,7 @@ let input = document.querySelector('#command');
 let submitBtn = document.querySelector('#execute');
 let pos = {
   row: 1,
-  col: 1,
+  col: 10,
   direction: 0
 };
 
@@ -26,20 +26,45 @@ function changePos() {
 function changeDirection() {
   block.style.transform = "rotate(" + pos.direction + "deg)";
 }
+
 function getCommand() {
   let command = input.value.toUpperCase();
   switch (command) {
     case 'TUN LEF':
       pos.direction  = pos.direction - 90 < 0 ? 270 : pos.direction - 90;
+      changeDirection();
       break;
     case 'TUN RIG':
-      pos.direction = pos.direction + 90 >= 360 ? 0 : pos.direction;
+      pos.direction = pos.direction + 90 >= 360 ? 0 : pos.direction + 90;
+      changeDirection();
       break;
     case 'TUN BAC':
       pos.direction += 180;
+      if (pos.direction === 360) {
+        pos.direction = 0;
+      }else if (pos.direction === 450) {
+        pos.direction = 90;
+      }
+      changeDirection();
+      break;
+    case 'GO':
+      switch(pos.direction) {
+        case 0:
+          pos.row = pos.row - 1 < 1 ? 1 : pos.row - 1;
+          break;
+        case 90:
+          pos.col = pos.col + 1 > 10 ? 10 : pos.col + 1;
+          break;
+        case 180:
+          pos.row = pos.row + 1 > 10 ? 10 : pos.row + 1;
+          break;
+        case 270:
+          pos.col = pos.col - 1 < 1 ? 1 : pos.col - 1;
+          break;
+      }
+      changePos();
       break;
   }
-  changeDirection();
 }
 
 submitBtn.addEventListener('click', getCommand);
@@ -47,4 +72,6 @@ submitBtn.addEventListener('click', getCommand);
 window.onload = function () {
   initPosition();
 };
+
+
 
