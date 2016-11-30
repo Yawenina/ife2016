2b9ac76b5a497172309b2b4e5c26457a29cd69b6
Yawenina <xiaoywkate@163.com>
2016-11-30 21:45:48 +0800

    implement textarea and validation

--- js/task35.js
+++ js/task35.js
@@ -1,18 +1,30 @@
 /**
  * Created by yawenina on 11/27/16.
  */
-var line_number = 1;
+var line_number = 0;
 var command_area = document.querySelector('.command-area');
 var line_number_elem = document.querySelector('.line-number');
 var refresh_btn = document.querySelector('#refresh');
 var exec_command_btn = document.querySelector('#execute');
 
 function setLineNumber(e) {
-  if (e.keyCode == 13) {
-    ++line_number;
-    let li = document.createElement('li');
-    li.textContent = line_number;
-    line_number_elem.appendChild(li);
+  let match = e.target.value.split('\n').length - 1;
+  if (match > line_number) {
+    let new_lines_count = match - line_number;
+    for (let i = 0; i < new_lines_count; i++) {
+      ++line_number;
+      let li = document.createElement('li');
+      li.textContent = line_number + 1;
+      line_number_elem.appendChild(li);
+    }
+  } else {
+    let new_lines_count = line_number - match;
+    console.log(new_lines_count);
+    for (let i = 0; i < new_lines_count; i++) {
+      --line_number;
+      let lastChild = line_number_elem.lastElementChild;
+      line_number_elem.removeChild(lastChild);
+    }
   }
 }
 
@@ -30,22 +42,29 @@ function clearCommandArea() {
 }
 
 function validateCommand() {
-  // TODO: add 'Go' and simplify  the procedure
   let isValidate = true;
   let command_arr = command_area.value.split('\n');
   for(let i = 0; i < command_arr.length; i++) {
     let curr_command = command_arr[i].toUpperCase().split(' ');
-    line_number_elem.children[i].className = '';
-    if (/[^TRA|MOV]/gi.test(curr_command[0])) {
-      line_number_elem.children[i].className = 'error-command';
+    let curr_number_elem = line_number_elem.children[i];
+    curr_number_elem.className = '';
+    if (/[^TRA|MOV|GO]/gi.test(curr_command[0])) {
+      curr_number_elem.className = 'error-command';
       isValidate = false;
       continue;
     }
-    if (/[^LEF|RIG|TOP|BOT]/gi.test(curr_command[1])) {
-      line_number_elem.children[i].className = 'error-command';
+    if (curr_command[0] === 'GO') {
+      if (/[^0-9]/.test(curr_command[1]) || curr_command.length > 2) {
+        curr_number_elem.className = 'error-command';
+        isValidate = false;
+      }
+      continue;
+    } else if (/[^LEF|RIG|TOP|BOT]/gi.test(curr_command[1])) {
+      curr_number_elem.className = 'error-command';
       isValidate = false;
       continue;
     }
+
     if (curr_command[2]) {
       if (/[^0-9]/.test(curr_command[2])) {
         line_number_elem.children[i].className = 'error-command';
@@ -56,7 +75,15 @@ function validateCommand() {
   return isValidate;
 }
 
+function execCommand() {
+  let isValidate = validateCommand();
+  if (isValidate) {
+
+  } else {
+    return false;
+  }
+}
 command_area.addEventListener('keyup', setLineNumber);
 command_area.addEventListener('scroll', setLineNumberScroll);
-exec_command_btn.addEventListener('click', validateCommand);
+exec_command_btn.addEventListener('click', execCommand);
 refresh_btn.addEventListener('click', clearCommandArea);
