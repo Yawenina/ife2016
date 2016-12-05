26db062c70466feac5ac35f7be8e804e72161465
Yawenina <xiaoywkate@163.com>
2016-12-05 21:58:30 +0800

    complete task 37

--- js/task37.js
+++ js/task37.js
@@ -1,16 +1,31 @@
 /**
  * Created by yawenina on 12/4/16.
  */
-let template = ` 
-    `
-
-options = {
-  width,
-  height,
-  title,
-  content,
+function $(selector) {
+  return document.querySelector(selector);
 }
 
-function CreateModal(id, options) {
-
+function hide(selector) {
+  $(selector).style.display = 'none';
 }
+
+function show(selector) {
+  $(selector).style.display = 'block';
+}
+
+$('body').addEventListener('click', function (e) {
+  let target = e.target;
+  if (target.dataset.toggle === 'modal') {
+    let modalId = target.dataset.target;
+    show(modalId);
+    $(modalId).addEventListener('click', function (e) {
+      let target = e.target;
+      if ( target.dataset.dismiss === 'modal' || ('#' + target.id) === modalId) {
+        hide(modalId)
+      } else {
+        return false
+      }
+    })
+  }
+});
+
