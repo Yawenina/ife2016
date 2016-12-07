3f79a97f48e6de42544c3b776ac9b29f3bbfbbc3
Yawenina <xiaoywkate@163.com>
2016-12-07 22:22:05 +0800

    complete task38

--- js/task38.js
+++ js/task38.js
@@ -20,14 +20,31 @@ function sortedTable(wrapperSelector, options) {
   this.thead = options.thead;
   this.data = options.data;
   this.sortCols = options.sortCols;
+  this.handlerFn = options.handlerFn
   this.table = null;
   this.tbody = null;
 }
 
-function bindEvent(elem, type) {
-  if (type === 'asc') {
-    elem.addEventListener('click')
+function sortColsFn(type, col, e){
+  return function(){
+    this.data.sort(function(a, b){
+      if (type === 'asc') {
+        return a[col] - b[col];
+      } else {
+        return b[col] - a[col];
+      }
+    });
+    this.renderTbody(this.data)
   }
+}
+
+
+// function bindEvent(elem, type, col) {
+//   elem.addEventListener('click', sortCol(type, col).bind(this));
+// }
+
+function bindEvent(elem, fn) {
+  elem.addEventListener('click', fn.bind(this));
 }
 
 function createBasicTable() {
@@ -48,8 +65,20 @@ function createBasicTable() {
       div.className = 'sort-arrow-wrapper';
       let ascSpan = document.createElement('span');
       ascSpan.className = 'asc';
+      if (this.handlerFn[0]) {
+        this.bindEvent(ascSpan, this.handlerFn[0])
+      } else {
+        this.bindEvent(ascSpan, this.sortColsFn('asc', i));
+      }
+
       let dscSpan = document.createElement('span');
       dscSpan.className = 'dsc';
+      if (this.handlerFn[1]) {
+        this.bindEvent(dscSpan, this.handlerFn[1])
+      } else {
+        this.bindEvent(dscSpan, this.sortColsFn('dsc', i));
+      }
+
       div.appendChild(ascSpan);
       div.appendChild(dscSpan);
       th.appendChild(div);
@@ -63,6 +92,7 @@ function createBasicTable() {
 }
 
 function renderTbody(data) {
+  this.tbody.innerHTML = ''
   data.forEach(row => {
     let tr = document.createElement('tr');
     let fragment = document.createDocumentFragment();
@@ -86,15 +116,18 @@ sortedTable.prototype = {
   constructor: sortedTable,
   createBasicTable,
   renderTbody,
-  init
+  init,
+  bindEvent,
+  sortColsFn
 }
 
 new sortedTable('.container', {
   thead: ['姓名', '语文', '数学', '英语', '总分'],
   data: [
     ['小红', 80, 81, 82, 83],
-    ['小明', 85, 72, 93, 100],
-    ['小黄', 99, 98, 96, 95]
+    ['小明', 99, 72, 93, 100],
+    ['小黄', 85, 98, 96, 95]
   ],
-  sortCols: ['语文', '总分']
+  sortCols: ['语文', '总分', '数学', '英语'],
+  handlerFn: []
 }).init();
