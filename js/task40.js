14d073f83537d8dcbe37b9d9c9987328673771af
Yawenina <xiaoywkate@163.com>
2016-12-10 12:29:15 +0800

    add task40

---
+++ js/task40.js
@@ -0,0 +1,26 @@
+/**
+ * Created by yawenina on 12/10/16.
+ */
+options = {
+  maxDate,
+  minData,
+}
+let selectionTemplate = `
+  
+`
+function $(selector) {
+  return document.querySelector(selector);
+}
+function Calender(wrapperSelector, options) {
+
+}
+
+function renderSelection() {
+
+}
+
+function init() {
+  let div = document.createElement('div');
+  div.className = 'calender';
+
+}
