f486b13fdc366f17d25ca85d948b1b4e7b97eeb4
Yawenina <xiaoywkate@163.com>
2017-01-05 23:08:02 +0800

    add task43

---
+++ js/task43.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yawenina on 1/5/17.
+ */
+
+function Gallery(wrapper, images) {
+  // TODO:未传参数的报错
+  this.wrapper = document.querySelector(wrapper);
+  this.images = images;
+  this.init();
+}
+
+Gallery.prototype.init = function () {
+
+}
+
+
