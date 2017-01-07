1c4a6e55887124b12f2636d005ff49ab5a74fdba
Yawenina <xiaoywkate@163.com>
2017-01-08 00:09:38 +0800

    add basic images display rules

--- js/task43.js
+++ js/task43.js
@@ -7,7 +7,7 @@ let oneImg = [
   "https://images.pexels.com/photos/175960/pexels-photo-175960.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
 ];
 let twoImgs = getImages(oneImg, 'https://images.pexels.com/photos/175554/pexels-photo-175554.jpeg?w=940&h=650&auto=compress&cs=tinysrgb');
-let threeImgs = getImages(twoImgs, "https://images.pexels.com/photos/175960/pexels-photo-175960.jpeg?w=940&h=650&auto=compress&cs=tinysrgb");
+let threeImgs = getImages(twoImgs, "https://images.pexels.com/photos/1440/city-road-street-buildings.jpg?h=350&auto=compress&cs=tinysrgb");
 let fourImgs = getImages(threeImgs, "https://images.pexels.com/photos/2324/skyline-buildings-new-york-skyscrapers.jpg?w=940&h=650&cs=tinysrgb");
 let fiveImgs = getImages(fourImgs, "https://images.pexels.com/photos/816/city-sky-skyline-sailboats.jpg?w=940&h=650&auto=compress&cs=tinysrgb");
 let sixImgs = getImages(fiveImgs, "https://images.pexels.com/photos/2324/skyline-buildings-new-york-skyscrapers.jpg?h=130&auto=compress&cs=tinysrgb");
@@ -16,5 +16,5 @@ new Gallery('.gallery-container', oneImg);
 new Gallery('.gallery-container', twoImgs);
 // new Gallery('.gallery-container', threeImgs);
 new Gallery('.gallery-container', fourImgs);
-new Gallery('.gallery-container', fiveImgs);
+// new Gallery('.gallery-container', fiveImgs);
 new Gallery('.gallery-container', sixImgs);
