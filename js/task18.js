d9a0a1ecbf1321c0f9dd4d442e1d1fb7acf1d660
Yawenina <xiaoywkate@163.com>
2017-01-23 22:29:12 +0800

    add leftin leftout rightin rightout handlers

--- js/task18.js
+++ js/task18.js
@@ -16,25 +16,46 @@ let handlers = {
 
 function updateQueue(e) {
   e.preventDefault();
+  let action = e.target.value;
   let val = input.value;
-  if (val < 10 || val > 100) {
-    alert("请输入10-100之间的数字！");
-    return false;
+  if (action === "leftIn" || action === "rightIn") {
+    if (val < 10 || val > 100) {
+      alert("请输入10-100之间的数字！");
+      return false;
+    }
+    if (queue.length > 60) {
+      alert (val);
+      return false;
+    }
   }
-  if (queue.length > 60) {
-    alert (val);
-    return false;
-  } else {
-    let action = e.target.value;
-    handlers[action](val);
-  }
+  handlers[action](val);
+  input.value = "";
 }
 
 function leftIn(val) {
   let span = document.createElement("span");
-  queue.unshift(span);
+  queue.unshift(val);
   span.style.height = val + "px";
   wrapper.insertBefore(span, wrapper.firstElementChild);
+}
+
+function rightIn(val) {
+  let span = document.createElement("span");
+  queue.push(val);
+  span.style.height = val + "px";
+  wrapper.appendChild(span, wrapper.firstElementChild);
+}
+
+function leftOut() {
+  const data = queue.shift();
+  wrapper.removeChild(wrapper.firstElementChild);
+  alert(data);
+}
+
+function rightOut() {
+  const data = queue.pop();
+  wrapper.removeChild(wrapper.lastElementChild);
+  alert(data);
 }
 
 btnGroup.addEventListener("click", updateQueue);
